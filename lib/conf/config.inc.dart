8c8d4e2d713578176f2980af21416e96d36539ef
FanFan <eric93504@gmail.com>
2020-11-14 13:21:12 +0800

    add config file

---
+++ lib/conf/config.inc.dart
@@ -0,0 +1,10 @@
+class Conf {
+  String apiPath;
+  String imagePath;
+
+  Conf({
+    this.imagePath = "http://163.17.135.152/Zesty/storage/",
+    this.apiPath = "http://163.17.135.152/Zesty/public/api/",
+
+  });
+}
