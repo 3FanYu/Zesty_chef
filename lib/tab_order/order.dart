5e9a4b5be435e29a6224effb2c43452dc9c12d14
FanFan <eric93504@gmail.com>
2020-11-28 19:55:28 +0800

    訂單頁tabbar

--- lib/tab_order/order.dart
+++ lib/tab_order/order.dart
@@ -13,10 +13,44 @@ class _NavBarState extends State<OrderPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('order'),
+      body: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: new PreferredSize(
+            preferredSize: Size.fromHeight(kToolbarHeight),
+            child: Container(
+              color: HexColor('#F1F1F1'),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    new Expanded(child: new Container()),
+                    TabBar(
+                      labelStyle: TextStyle(
+                        fontSize: 18.0,
+                      ),
+                      labelColor: HexColor('#727272'),
+                      tabs: [
+                        Tab(
+                          text: '待確認',
+                        ),
+                        Tab(text: '即將到來'),
+                        Tab(text: '已完成'),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              Icon(Icons.local_laundry_service),
+              Icon(Icons.ac_unit),
+              Icon(Icons.do_not_disturb),
+            ],
+          ),
+        ),
       ),
-      backgroundColor: HexColor('#727272'),
     );
   }
 }
